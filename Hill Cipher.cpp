e59947f15ef0684767e449c7d3ca89fb6a0c5830
Prasad <psdudeman39@gmail.com>
2020-02-03 17:31:22 +0530

    Decryption logic is written for Hill Cippher algorithm. Decryption is not working properly. Need to look into that

--- Hill Cipher.cpp
+++ Hill Cipher.cpp
@@ -5,6 +5,19 @@ const int mod = 65;
 int n;
 vector< vector<int> > keyMatrix;
 //string gfg_key = "GYBNQKURP";
+
+template<typename T>
+void printMatrix(vector<vector<T>> matrix){
+	cout<<"=============Printing Matrix====================="<<endl;
+	for(int i = 0; i < matrix.size(); i++){
+		for(int j = 0; j < matrix[0].size(); j++){
+			cout<<matrix[i][j]<<" ";
+		}
+		cout<<endl;
+	}
+	cout<<"================================================="<<endl;
+}
+
 void initializeKeyMatrix(vector<vector<int>> &keyMatrix, int sz = 1){
 //	int cnt = 0;
 	for(int i = 0; i < keyMatrix.size(); i++){
@@ -47,32 +60,72 @@ int determinant(vector<vector<int>> &matrix, int n){
 	return det;
 }
 
-vector<vector<int> > matrixInverse(vector<vector<int>> &matrix){
-	vector<vector<int> > inverseMatrix(matrix.size(), vector<int>(keyMatrix[0].size()));
+vector<vector<int>> getAdjointMatrix(vector<vector<int>> &matrix){
+	int n = matrix.size();
+	vector<vector<int>> adjointMatrix(n, vector<int>(n));
+	if(n == 1){
+		adjointMatrix[0][0] = 1;
+		return adjointMatrix;
+	}
 
-	//	calculate determinant
+	int sign = 1;
+	vector<vector<int>> cofactor(n, vector<int>(n));
+	for(int i = 0; i < n; i++){
+		for(int j = 0; j < n; j++){
+			createCofactor(matrix, i, j, cofactor);
+			sign = ((i+j)&1 ? -1 : 1);
+			adjointMatrix[i][j] = sign * determinant(cofactor, n-1);
+		}
+	}
+
+	return adjointMatrix;
+}
+
+vector<vector<float> > matrixInverse(vector<vector<int>> &matrix){
+//	vector<vector<int> > inverseMatrix(matrix.size(), vector<int>(keyMatrix[0].size()));
+
 	int det = determinant(matrix, matrix.size());
-	cout<<"det: "<<det<<endl;
 
-	//	calculate adjoint
+	vector<vector<int>> adjointMatrix = getAdjointMatrix(matrix);
 
 	//	find the inverse
+	vector<vector<float>> inverseMatrix(matrix.size(), vector<float>(matrix[0].size()));
+	for(int i = 0; i < n; i++){
+		for(int j = 0; j < n; j++){
+			inverseMatrix[i][j] = (float)(adjointMatrix[i][j])/(float)(det);
+		}
+	}
 
-	return vector<vector<int>>();
+	return inverseMatrix;
 }
 
 vector<int> encrypt(vector<int> msgMatrix, vector<vector<int>> keyMatrix){
-	vector<int> cipherMsg(n);
+	vector<int> cipherMatrix(n);
 
 	for(int i = 0; i < n; i++){
-		cipherMsg[i] = 0;
+		cipherMatrix[i] = 0;
 		for(int x = 0; x < n; x++){
-			cipherMsg[i] += keyMatrix[i][x] * msgMatrix[x];
+			cipherMatrix[i] += keyMatrix[i][x] * msgMatrix[x];
 		}
-		cipherMsg[i] = cipherMsg[i]%26;
+		cipherMatrix[i] = cipherMatrix[i]%26;
 	}
 
-	return cipherMsg;
+	return cipherMatrix;
+}
+
+vector<float> decrypt(vector<int> &encryptedMatrix,
+								vector<vector<float>> &inverseMatrix){
+	int sz = encryptedMatrix.size();
+	vector<float> decryptedMatrix(sz);
+	for(int i = 0; i < sz; i++){
+		for(int x = 0; x < sz; x++){
+			decryptedMatrix[i] += inverseMatrix[i][x] * (float)encryptedMatrix[x];
+		}
+		decryptedMatrix[i] = (char)(fmod(decryptedMatrix[i], 26) + 'A');
+		cout<<"decryp[i]: "<<decryptedMatrix[i]<<endl;
+	}
+
+	return decryptedMatrix;
 }
 
 int main(){
@@ -84,6 +137,7 @@ int main(){
 	keyMatrix = vector<vector<int>>(n, vector<int>(n));
 	initializeKeyMatrix(keyMatrix, n);
 
+	//	Encryption
 	vector<int> msgMatrix(msg.begin(), msg.end());
 	for(int i = 0; i < msgMatrix.size(); i++) msgMatrix[i] = msgMatrix[i]%mod;
 	vector<int> encryptedMatrix = encrypt(msgMatrix, keyMatrix);
@@ -92,8 +146,13 @@ int main(){
 	cout<<"Encrypted msg: "<<encryptedMsg<<endl;
 	cout<<endl;
 
-	//	Decryption remaining...
-	vector<vector<int> > inverseKeyMatrix(keyMatrix.size(), vector<int>(keyMatrix[0].size()));
-	inverseKeyMatrix = matrixInverse(keyMatrix);
+	//	Decryption
+	vector<vector<float> > inverseKeyMatrix = matrixInverse(keyMatrix);
+	printMatrix(inverseKeyMatrix);
+	vector<float> decryptedMatrix = decrypt(encryptedMatrix, inverseKeyMatrix);
+	cout<<"Decrypted string: ";
+	for(auto it: decryptedMatrix) cout<<(char)it;
+
+
 	return 0;
 }
